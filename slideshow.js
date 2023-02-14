9f3de407b5bd9f410a281dd6e6eb5b3879a90942
Stephen Kerschbaumer <stephenkerschbaumer@gmail.com>
2023-02-13 18:53:00 -0800

    Added header, slideshow
    Header, slideshow, minor CSS tweaks

---
+++ slideshow.js
@@ -0,0 +1,21 @@
+//SLIDESHOW TUTORIAL: https://www.w3schools.com/howto/howto_js_slideshow.asp
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" dot_active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " dot_active";
+  setTimeout(showSlides, 2000); //milliseconds
+}
