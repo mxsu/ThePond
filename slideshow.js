ebf00174cb3e8cebe9696ff6a0aa6a7c95ddf318
Stephen Kerschbaumer <stephenkerschbaumer@gmail.com>
2023-02-23 14:48:00 -0800

    Slideshow buttons, footer
    The slideshow auto scrolls and can be controlled via buttons. All pages were given a basic footer

--- slideshow.js
+++ slideshow.js
@@ -1,21 +1,64 @@
 //SLIDESHOW TUTORIAL: https://www.w3schools.com/howto/howto_js_slideshow.asp
 
 let slideIndex = 0;
-showSlides();
+showSlides(slideIndex);
+autoSlide(slideIndex);
 
-function showSlides() {
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("slide");
   let dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
+
+  if (slideIndex > slides.length) {slideIndex = 1}   
+
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" dot_active", "");
   }
+
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " dot_active";
-  setTimeout(showSlides, 4000); //milliseconds
 }
+
+function autoSlide(n){
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dot");
+
+  plusSlides(1);
+
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+
+  if (slideIndex > slides.length) {slideIndex = 1}   
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" dot_active", "");
+  }
+
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " dot_active";
+
+  setTimeout(function() {autoSlide(slideIndex)}, 4000); //repeat in 4 seconds
+}
+
